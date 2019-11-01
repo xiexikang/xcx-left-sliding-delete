0f2fc6b9b54ab72c5d4d76af711958f71440ddc5
xiexikang <1371419734@qq.com>
2019-11-01 15:08:24 +0800

    优化滑动删除功能

--- xcx-left-del/xcx-slide-left-del/pages/collect/collect.js
+++ xcx-left-del/xcx-slide-left-del/pages/collect/collect.js
@@ -32,31 +32,28 @@ Page({
         "type": "1"
       },
     ],
-    startX: 0, //开始坐标
-    startY: 0
+    startX: 0, //开始坐标x
+    startY: 0, //开始坐标y
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  touchstart: function (e) {
-    //开始触摸时 重置所有删除
-    this.data.items.forEach(function (v, i) {
-      if (v.isTouchMove)//只操作为true的
+  
+  //开始触摸时
+  touchstart(e) {
+    var that = this;
+    that.data.items.forEach(function (v, i) {
+      if (v.isTouchMove){
         v.isTouchMove = false;
+      }
     })
-    this.setData({
+    that.setData({
       startX: e.changedTouches[0].clientX,
       startY: e.changedTouches[0].clientY,
-      items: this.data.items
+      items: that.data.items
     })
   },
+
   //滑动事件处理
-  touchmove: function (e) {
+  touchmove(e) {
     var that = this,
       index = e.currentTarget.dataset.index,//当前索引
       startX = that.data.startX,//开始X坐标
@@ -65,41 +62,73 @@ Page({
       touchMoveY = e.changedTouches[0].clientY,//滑动变化坐标
       //获取滑动角度
       angle = that.angle({ X: startX, Y: startY }, { X: touchMoveX, Y: touchMoveY });
+
     that.data.items.forEach(function (v, i) {
       v.isTouchMove = false
       //滑动超过30度角 return
       if (Math.abs(angle) > 30) return;
       if (i == index) {
-        if (touchMoveX > startX) //右滑
-          v.isTouchMove = false
-        else //左滑
-          v.isTouchMove = true
+        if (touchMoveX > startX){
+          v.isTouchMove = false;  //右滑
+        }else{
+          v.isTouchMove = true; //左滑
+        } 
       }
     })
-    //更新数据
+   
     that.setData({
       items: that.data.items
     })
   },
-  /**
-   * 计算滑动角度
-   * @param {Object} start 起点坐标
-   * @param {Object} end 终点坐标
-   */
-  angle: function (start, end) {
+ 
+  // 计算滑动角度 start 起点坐标  end 终点坐标
+  angle(start, end) {
     var _X = end.X - start.X,
       _Y = end.Y - start.Y
     //返回角度 /Math.atan()返回数字的反正切值
     return 360 * Math.atan(_Y / _X) / (2 * Math.PI);
   },
-  //删除事件
-  del: function (e) {
-    this.data.items.splice(e.currentTarget.dataset.index, 1)
-    this.setData({
-      items: this.data.items
+
+  //取消收藏
+  cancleCollect(e) {
+    var that = this,
+        index = e.currentTarget.dataset.index,
+        items = that.data.items;
+    items[index].isTouchMove = true; 
+    that.setData({
+      items: items
     })
+    wx.showModal({
+      title: '温馨提示',
+      content: '亲，您确定要取消此收藏吗？',
+      success(res) {
+        if (res.confirm) {
+          items.splice(index, 1);
+          that.setData({
+            items: items
+          })
+          wx.showToast({
+            title: '取消收藏成功~',
+            icon: 'success',
+            duration: 1500
+          })
+        } else if (res.cancel) {
+          items[index].isTouchMove = false;
+          that.setData({
+            items: items
+          })
+        }
+      }
+    })
+  
   },
 
+  /**
+     * 生命周期函数--监听页面加载
+     */
+  onLoad: function (options) {
+
+  },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
